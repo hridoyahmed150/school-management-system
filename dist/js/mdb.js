438a1d08ff58c2cc6fa0004e183752aa7c3611ff
Hridoy <hridoyahmed150@gmail.com>
2018-09-05 12:18:51 +0600

    2nd day

--- dist/js/mdb.js
+++ dist/js/mdb.js
@@ -1,54 +1,10 @@
-/*!
- * Material Design for Bootstrap 4
- * Version: MDB FREE: 4.5.9
- *
- *
- * Copyright: Material Design for Bootstrap
- * https://mdbootstrap.com/
- *
- * Read the license: https://mdbootstrap.com/license/
- *
- *
- * Documentation: https://mdbootstrap.com/
- *
- * Getting started: https://mdbootstrap.com/getting-started/
- *
- * Tutorials: https://mdbootstrap.com/bootstrap-tutorial/
- *
- * Templates: https://mdbootstrap.com/templates/
- *
- * Support: https://mdbootstrap.com/forums/forum/support/
- *
- * Contact: office@mdbootstrap.com
- *
- * Atribution: Animate CSS, Twitter Bootstrap, Materialize CSS, Normalize CSS, Waves JS, WOW JS, Toastr, Chart.js , Hammer.js
- *
- */
-
-
-/*
-
-  jquery.easing.js
-  velocity.min.js
-  chart.js
-  wow.js
-  scrolling-navbar.js
-  waves.js
-  forms-free.js
-  enhanced-modals.js
-
-*/
-// Waves.attach('.button');
-// Waves.attach('.button', ['waves-button', 'waves-float']);
-(function($){
-	$('.panel').on('click',function(){
-
-		$(this).animate({
-			width:70%,
-			opacity:.4
-		});
-	})
-})(jQuery)
+(function(){
+	$('.container-fluid>.row>.panel').on('click',function(){
+		var $this=$(this);
+		$this.toggleClass('open').delay(400).toggleClass("open-active").siblings().removeClass('open open-active');
+	});
+	
+})();
 /*
  * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
  *
--- js/_intro-mdb-free.js
+++ js/_intro-mdb-free.js
@@ -1,1 +1,7 @@
-
+(function(){
+	$('.container-fluid>.row>.panel').on('click',function(){
+		var $this=$(this);
+		$this.toggleClass('open').delay(400).toggleClass("open-active").siblings().removeClass('open open-active');
+	});
+	
+})();
